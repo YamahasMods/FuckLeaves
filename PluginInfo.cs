0aeec4b00e8b0043ad73d0dfa8fc62887c6ff638
Yamaha <ari.arikid@gmail.com>
2024-05-08 16:57:19 -0700

    Create PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZenonsModTemplate
+{
+    public class PluginInfo
+    {
+        public const string GUID = "YAMAHA.FUCKLEAVES";
+        public const string Name = "YAMAHAs FUCK LEAVES MOD";
+        public const string Version = "1.0.0";
+    }
+}
